c12c3ec03a55e46399e9001a4091edc346859b16
Katie <katherinelane11@gmail.com>
2017-07-24 09:41:51 -0400

    cleaned up HTML classes

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,6 +1,9 @@
 var MatchGame = {};
 var $BOX = $('.box-row-1');
 var $CARD_INFO = $('.card-info')
+var $game = []
+var cardFlipped = false;
+var data = []
 // var $myData = $(".card-numbers");
 /*
   Sets up a new game after HTML document has loaded.
@@ -11,15 +14,16 @@ var $CARD_INFO = $('.card-info')
   Generates and returns an array of matching card values.
  */
 
-MatchGame.generateCardValues = function () {
+//  generates random numbers from two sets of values ranged 1-8. this created the "pair" effect - the final array has two 1s, two 2s, etc. and in random order
+function generateCardValues() {
   var matchOne = [8, 7, 6, 5, 4, 3, 2 , 1];
   var matchTwo = [8, 7, 6, 5, 4, 3, 2 , 1];
   var concatMatch = matchOne.concat(matchTwo);
   var randomArray = shuffleArray(concatMatch);
-  var $game;
-  MatchGame.renderCards(randomArray, $game);
+  return randomArray;
 };
 
+// shuffles the array so in random order
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i --) {
     var j = Math.floor(Math.random() * (i + 1))
@@ -29,21 +33,36 @@ function shuffleArray(array) {
   };
   return array;
 };
+
+
 /*
   Converts card values to jQuery card objects and adds them to the supplied game
   object.
 */
 
-MatchGame.renderCards = function(cardValues, $game) {
-  // $game.empty();
+// did nothing
+// renders cards to get 
+// function renderCards(cardValues) {
+//   var newArray = giveCardValue(cardValues);
+// };
+
+
+
+function giveCardValue(cardValues){
+  // loops through eachbox and uses .text to assign one of the numberes in the random array.
+  // based on those values, the background color is set so 1 and 1 have the same color. etc,
+
   $BOX.each( function (i){
     $(this).text(cardValues[i]);
-    colorBackground($(this), cardValues[i]);
+    var value = colorBackground($(this), cardValues[i])
+    console.log(value)
   });
-  
+  // return data
 };
 
+// takes $BOX which is $('.box-row-1'); and loops through each box's value to find what background color should do.
 function colorBackground(box, value) {
+  $.each(box, function (i){
     if (value === 1){
       box.css("background-color", "yellow")
     } else if (value === 2) {
@@ -59,28 +78,49 @@ function colorBackground(box, value) {
     } else if (value === 7) {
       box.css("background-color", "black")
     } 
+  })
+    
+    return value;
   };
 
-
+// function cardFlip(){
+  
 
 /*
   Flips over a given card and checks to see if two cards are flipped over.
   Updates styles on flipped cards depending whether they are a match or not.
  */
 
+//  when card is fliped, etc.
+// function detectCardFlip(){
+//   $('[data-card="card-numbers"]').on('click', function (){
+//     event.preventDefault();
+//     cardFlipped = true;
+//     $('[data-card="card-numbers"]').show()
+//   });
+// }
+
+
+
 MatchGame.flipCard = function($card, $game) {
+  
 
 };
 
-MatchGame.generateCardValues();
+
+
+function restartGame(){
+    $('[data-type-button="submit"]').on('click', function (){
+        event.preventDefault();
+        var array = generateCardValues();
+        giveCardValue(array);
+    });
+};
 
 
 
 
-
-
-
-
+restartGame();
 
 
 // var dataLayer = value.data('card');
